36fc7a536d6462bd7b904017d4c4aa3f6e342550
Dave Lim <dlimiter@gmail.com>
2010-12-05 20:55:21 +1100

    Minor adjustments to text and layout.

--- push_site.rb
+++
@@ -1,9 +0,0 @@
-require 'net/ftp'
- 
-ftp = Net::FTP::new("dlimiter.net")
-ftp.login("anonymous", "dlimiter")
-ftp.chdir("/pub/ruby")
-tgz = ftp.list("ruby-*.tar.gz").sort.last
-print "the latest version is ", tgz, "\n"
-ftp.getbinaryfile(tgz, tgz)
-ftp.close
