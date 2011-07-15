07ab794fb06d2d07ad73e00838d269f884f9e33b
Dave Lim <dlimiter@gmail.com>
2011-07-15 15:09:24 +1000

    Added Contact from to the footer with associated php scripts. Some general site styling. Extruded footer elements to includes.

---
+++ bin/mail.php
@@ -0,0 +1,63 @@
+<?php
+if ((isset($_POST['name'])) && (strlen(trim($_POST['name'])) > 0)) {
+	$name = stripslashes(strip_tags($_POST['name']));
+} else {$name = 'No name entered';}
+if ((isset($_POST['email'])) && (strlen(trim($_POST['email'])) > 0)) {
+	$email = stripslashes(strip_tags($_POST['email']));
+} else {$email = 'No email entered';}
+if ((isset($_POST['message'])) && (strlen(trim($_POST['message'])) > 0)) {
+	$message = stripslashes(strip_tags($_POST['message']));
+} else {$message = 'No message entered';}
+ob_start();
+?>
+<html>
+  <head>
+  <style type="text/css">
+  </style>
+  </head>
+  <body>
+    <p>
+Hey Dave, <?=$name;?> (<?=$email;?>) sent you a message:
+    </p>
+    <p>
+<?=$message;?>
+    </p>
+  </body>
+</html>
+
+<?php
+$body = ob_get_contents();
+include_once('class.phpmailer.php');
+
+$mail = new PHPMailer();
+
+$to = 'dave@dlimiter.net';
+$toname = 'Dave';
+$fromaddress = "formbot@dlimiter.net";
+$fromname = "Online Contact Form (dlimiter.net)";
+
+$mail->IsSMTP();
+
+$mail->SMTPAuth = true;
+$mail->Host = "mail.dlimiter.net";
+$mail->Username = "formbot+dlimiter.net";
+$mail->Password =  "MrFl1bbl3!";
+$mail->Port = 587;
+
+$mail->From     = $fromaddress;
+$mail->FromName = $fromname;
+$mail->AddAddress($to,$toname);
+
+$mail->WordWrap = 50;
+$mail->IsHTML(true);
+
+$mail->Subject  =  "Online Contact From Submitted (dlimiter.net)";
+$mail->Body     =  $body;
+
+if(!$mail->Send()) {
+	$content = $body;	
+	echo $mail->ErrorInfo;
+  mail($to, $subject, 'Contact form failed', "From: $fromaddress\r\nReply-To: $content\r\nX-Mailer: DT_formmail");
+  exit;
+}
+?>
--- js/site.js
+++ js/site.js
@@ -3,7 +3,6 @@
 
 $(document).ready(function () {
   /* Apply fancybox to multiple items */
-  console.log(">>"+document.title);
   if (document.title.search('Portfolio') != -1) {
     $("a.folio-image-wrapper").fancybox({
      'transitionIn'   : 'elastic',
@@ -16,7 +15,55 @@ $(document).ready(function () {
      'titleFormat'    : formatTitle
     });
   }
+    
+  $('.error').hide();      
+  // Hide any validation error elements
+  
+  $("input#submit-btn").click(function() {
+    // Rehide error messages
+    $('.error').hide();  
+    var name = $("input#name").val();  
+    if (name == "") {  
+      $("#name-error").show();  
+      $("input#name").focus();  
+      return false;  
+    }  
+    var email = $("input#email").val();  
+    if (!isValidEmail(email)) {  
+      $("#email-error").show();  
+      $("input#email").focus();  
+      return false;  
+    }  
+    var message = $("textarea#message").val();  
+    if (message == "") {  
+      $("#message-error").show();  
+      $("input#message").focus();  
+      return false;  
+    }
+    
+    // Send
+    var dataString = 'name='+ name + '&email=' + email + '&message=' + message;
+    //alert (dataString);return false;
+    $.ajax({
+      type: "POST",
+      url: "/bin/mail.php",
+      data: dataString,
+      success: function() {
+        $('#contact-form').html("<div id='success-message'></div>");
+        $('#success-message').html("<p>Hey, thanks for the message!</p>").append("<p>I'll be in touch soon.</p>").hide().fadeIn(1500, function() {
+          $('#message').append("Zark");
+        });
+      } 
+    });
+    return false;
+  });    
+    
 });
+
+function isValidEmail(email) { 
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ 
+ return email.match(re) 
+}
 
 function formatTitle(title, currentArray, currentIndex, currentOpts) {
     return '<div id="fancybox-title"><span id="closer"><a href="javascript:;" onclick="$.fancybox.close();"><img src="/js/fancybox/closelabel.gif" /></a></span>' + (title && title.length ? '<span id="title-label">' + title + '</span>' : '' ) +  '</div>';
