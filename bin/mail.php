001027254e1b54ac1c7df4e1b48d64d22ee1cd87
Dave Lim <dlimiter@gmail.com>
2011-07-17 22:28:08 +1000

    Neatened up script

--- bin/mail.php
+++ bin/mail.php
@@ -1,13 +1,23 @@
 <?php
+$dodgy = false;
 if ((isset($_POST['name'])) && (strlen(trim($_POST['name'])) > 0)) {
 	$name = stripslashes(strip_tags($_POST['name']));
-} else {$name = 'No name entered';}
+} else {
+  $name = 'No name entered';
+  $dodgy = true;
+}
 if ((isset($_POST['email'])) && (strlen(trim($_POST['email'])) > 0)) {
 	$email = stripslashes(strip_tags($_POST['email']));
-} else {$email = 'No email entered';}
+} else {
+  $email = 'No email entered';
+  $dodgy = true;
+}
 if ((isset($_POST['message'])) && (strlen(trim($_POST['message'])) > 0)) {
 	$message = stripslashes(strip_tags($_POST['message']));
-} else {$message = 'No message entered';}
+} else {
+  $message = 'No message entered';
+  $dodgy = true;
+}
 ob_start();
 ?>
 <html>
@@ -31,18 +41,16 @@ include_once('class.phpmailer.php');
 
 $mail = new PHPMailer();
 
-$to = 'dave@dlimiter.net';
-$toname = 'Dave';
-$fromaddress = "formbot@dlimiter.net";
-$fromname = "Online Contact Form (dlimiter.net)";
-
+$to             = 'dave@dlimiter.net';
+$toname         = 'Dave';
+$fromaddress    = "formbot@dlimiter.net";
+$fromname       = "Online Contact Form (dlimiter.net)";
 $mail->IsSMTP();
-
 $mail->SMTPAuth = true;
-$mail->Host = "mail.dlimiter.net";
+$mail->Host     = "mail.dlimiter.net";
 $mail->Username = "formbot+dlimiter.net";
 $mail->Password =  "MrFl1bbl3!";
-$mail->Port = 587;
+$mail->Port     = 587;
 
 $mail->From     = $fromaddress;
 $mail->FromName = $fromname;
